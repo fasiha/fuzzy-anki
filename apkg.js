0cf12288215d50a0f1ca5c6997faf07338b4b71d
Ahmed Fasih <wuzzyview@gmail.com>
2014-08-22 01:46:29 -0400

    Updating documentation

--- apkg.js
+++ apkg.js
@@ -113,23 +113,82 @@ $(document).ready(function() {
 
     $("#ankiFile").change(eventHandleToTable);
 
-    ankiURLToTable('/n.apkg');
+    // ankiURLToTable('/n.apkg');
 });
 
+/**
+* Hook that modifies Nayr's Japanese Core5000 Anki deck
+* (see https://ankiweb.net/shared/info/631662071)
+*
+* @param {Array} deckNotes - array of Anki Notes from the above deck
+* @param {String[]} deckFields - names of the fields of the Notes
+* @return {Array} an updated version of deckNotes
+*
+* Each Note object containing properties Expression, Meaning, Reading, English
+* Translation, Word, Frequency Order, and Sound.
+*
+* Kana in the "Reading" field will be changed from "[kana]" to being wrapped in
+*<span> tags. And each of the items in the "Word" field, which contains the
+*Japanese word, its reading in roumaji (Latin characters), one or more
+*parts-of-speech, and English translations, will be encased in <span> tags
+*(ideally these would be their own independent fields, but some rows have more
+*than one part-of-speech).
+*/
 function core5000Modify(deckNotes, deckFields) {
-    var kanaRegexp = XRegExp('\\[([\\p{Hiragana}\\p{Katakana}]+)\\]', 'g');
-
+    //------------------------------------------------------------
+    // Variables and functions to help deal with the "Word" column
+    //------------------------------------------------------------
+    // Parts of speech abbreviations
     var abbreviations =
         "adn.,adv.,aux.,conj.,cp.,i-adj.,interj.,n.,na-adj.,num.,p.,p. \
 case,p. conj.,p. disc.,pron.,v.".split(',');
     var abbreviationsOr = abbreviations.join("|").replace(/\./g, '\\.');
 
+    // The basic structure of the "Word" column is:
+    //
+    // 1. some kanji or kana, plus other random things like commas, parentheses,
+    // both ascii and full-width.
+    // 2. Some roumaji
+    // 3. One or more parts of speech, using the above abbreviations
+    // 4. English translations.
+    //
+    // The following three strings will be the regexps that match #1--#3.
+    // They've been carefully chosen to work with wrinkles in the database,
+    // e.g., more than one of the above four-step sequences in a single row,
+    // multiple adjacent parts-of-speech, or multiple
+    // part-of-speech-and-translation pairs.
     var kanaKanjiWordRegexp = '([^a-z]+)';
     var romajiRegexp = '([a-z\\s,\\-()’]+)';
     var partOfSpeechRegexp = '((?: |,|' + abbreviationsOr + ')+)';
 
-    var bar = function(s) {
-        var arr = s.split(XRegExp('(' + abbreviationsOr + ')'));
+    // This string's regexp is used to separate multiple sequences of #1--#4
+    // above. All these strings intended to become regexps will go through
+    // XRegExp, which expands out the Han/Katakana/Hiragana groups.
+    var completeWordRegexp =
+        '([^a-z]+)([^\\p{Han}\\p{Katakana}\\p{Hiragana}]+)';
+
+    // Break up a string containing one {kanji/kana + roumaji + part-of-speech +
+    // translations} sequence. The critical idea in this function is to split
+    // the input string between part-of-speech-abbreviations, and do some
+    // processing on that to handle two edge cases:
+    //
+    // 1. "いろいろ iroiro adv., na-adj. various" <-- more than one adjacent
+    //     part-of-speech abbreviation separated by a comma
+    // 2. "余り amari adv. the rest n. (not) much" <-- more than one
+    //     part-of-speech/translation pairs.
+    //
+    // It handles both these cases by splitting the string into an array along
+    // (and including) part-of-speech-abbreviation boundaries. To handle edge
+    // case 1 above, it finds elements of the resulting array that are
+    // between part-of-speech abbreviations but which are
+    // whitespace/punctuation, and merges those elements into a single
+    // "part-of-speech" element.
+    //
+    // Then it builds an array of parts-of-speech and a matching array of
+    // translations. This handles case 2 above. These two arrays, as well as the
+    // kanji/kana and roumaji, are returned as an object.
+    function bar(seqString) {
+        var arr = seqString.split(XRegExp('(' + abbreviationsOr + ')'));
 
         var isAbbreviation = arr.map(
             function(x) { return abbreviations.indexOf(x) >= 0 ? 1 : 0; });
@@ -138,10 +197,17 @@ case,p. conj.,p. disc.,pron.,v.".split(',');
         var isAbbrOrWhitePunct = isAbbreviation.map(
             function(x, i) { return x + isWhitePunctuation[i]; });
 
-        var tmp = removeJunk(isAbbrOrWhitePunct, arr);
-        arr = tmp.other_array;
-        isAbbrOrWhitePunct = tmp.this_array;
+        // combineJunk will find [..., "adv.", ",", "na-adj.", ...] and splice
+        // it into [..., "adv., na-adj.", ...].
+        var tmp = combineJunk(isAbbrOrWhitePunct, arr);
+        arr = tmp.data_array;
+        isAbbrOrWhitePunct = tmp.indicator_array;
+        // Updated arr and isAbbrOrWhitePunct. We need the latter to build the
+        // return object.
 
+        // Part-of-speech array and translation array, which will go itno the
+        // return object. We rely on each part-of-speech element in arr to be
+        // followed by a translation. So far, this happens.
         var pos = [];
         var translation = [];
         arr.map(function(x, i) {
@@ -151,9 +217,12 @@ case,p. conj.,p. disc.,pron.,v.".split(',');
             }
         });
 
+        // Grab the initial kanji/kana as well as the roumaji. String.match()
+        // will return a three-element array here: the total match, and the two
+        // groups corresponding to the two regexps.
         var kanaKanjiMatch =
-            s.match(XRegExp(kanaKanjiWordRegexp + ' ' + romajiRegexp + ' ' +
-                            partOfSpeechRegexp));
+            seqString.match(XRegExp(kanaKanjiWordRegexp + ' ' + romajiRegexp +
+                                    ' ' + partOfSpeechRegexp));
         return {
             pos : pos,
             translation : translation,
@@ -162,18 +231,18 @@ case,p. conj.,p. disc.,pron.,v.".split(',');
         };
     };
 
-    function removeJunk(this_array, other_array) {
+    function combineJunk(indicator_array, data_array) {
         var i = 1;
-        while (i < this_array.length) {
-            if (this_array[i] == this_array[i - 1] && this_array[i] > 0) {
-                this_array.splice(i - 1, 2, 1);
-                other_array.splice(i - 1, 2,
-                                   other_array[i - 1] + other_array[i]);
+        while (i < indicator_array.length) {
+            if (indicator_array[i] == indicator_array[i - 1] && indicator_array[i] > 0) {
+                indicator_array.splice(i - 1, 2, 1);
+                data_array.splice(i - 1, 2,
+                                   data_array[i - 1] + data_array[i]);
             } else {
                 i++;
             }
         }
-        return {other_array : other_array, this_array : this_array};
+        return {data_array : data_array, indicator_array : indicator_array};
     }
 
     function decodeHtml(html) {
@@ -186,8 +255,7 @@ case,p. conj.,p. disc.,pron.,v.".split(',');
         if (s.search("&") >= 0) {
             s = decodeHtml(s);
         }
-        var arr = s.match(
-            XRegExp('([^a-z]+)([^\\p{Han}\\p{Katakana}\\p{Hiragana}]+)', 'g'));
+        var arr = s.match(XRegExp(completeWordRegexp, 'g'));
 
         return arr.map(function(s) {
             var decomp = bar(s);
@@ -202,19 +270,30 @@ case,p. conj.,p. disc.,pron.,v.".split(',');
         }).join("");
     };
 
+    //--------------------------------------------
+    // Variable for Reading column cleanup of kana
+    //--------------------------------------------
+    var kanaRegexp = XRegExp('\\[([\\p{Hiragana}\\p{Katakana}]+)\\]', 'g');
+
+    //-----------------
+    // Complete cleanup
+    //-----------------
     deckNotes.map(function(note, loc, arr) {
+        // Break up Word column into its four separate components
+        note.Word = wordColumnReplace(note.Word);
+
         // Replace [kana] with spans
         note.Reading = note.Reading.replace(kanaRegexp,
                                             function(match, kana, offset, str) {
             return '<span class="reading kana">' + kana + '</span>';
         });
 
-        // Break up Word column into its four separate components
-        note.Word = wordColumnReplace(note.Word);
-
         return note;
     });
 
+    //-------------------------
+    // Visualization and return
+    //-------------------------
     d3.select("body").append("div").attr("id", "core5000");
     tabulate(deckNotes, deckFields, "#core5000");
 
