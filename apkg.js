7c0087886ae569d0c987fc8bea7250a9581d27e7
Ahmed Fasih <wuzzyview@gmail.com>
2014-08-21 23:31:16 -0400

    Tag parts of the Word column.

--- apkg.js
+++ apkg.js
@@ -16,15 +16,11 @@ function tabulate(datatable, columns, containerString) {
     var rows = tbody.selectAll("tr").data(datatable).enter().append("tr");
 
     // create a cell in each row for each column
-    var cells = rows.selectAll("td")
-                    .data(function(row) {
-                        return columns.map(function(column) {
-                            return {column : column, value : row[column]};
-                        });
-                    })
-                    .enter()
-                    .append("td")
-                    .html(function(d) { return d.value; });
+    var cells = rows.selectAll("td").data(function(row) {
+        return columns.map(function(column) {
+            return {column : column, value : row[column]};
+        });
+    }).enter().append("td").html(function(d) { return d.value; });
 
     return table;
 }
@@ -61,7 +57,7 @@ function sqlToTable(uInt8ArraySQLdb) {
             myObj[deckFields[i]] = facts[i];
         }
         return myObj;
-    }
+    };
     deckNotes[0].values.forEach(
         function(val) { notes.push(arrayToObj(val[0].split(ankiSeparator))); });
     deckNotes = notes;
@@ -88,7 +84,7 @@ function ankiURLToTable(ankiURL) {
     var zipxhr = new XMLHttpRequest();
     zipxhr.open('GET', ankiURL, true);
     zipxhr.responseType = 'arraybuffer';
-    zipxhr.onload = function(e) { ankiBinaryToTable(this.response); }
+    zipxhr.onload = function(e) { ankiBinaryToTable(this.response); };
     zipxhr.send();
 }
 
@@ -103,9 +99,7 @@ $(document).ready(function() {
         // console.log(f.name);
 
         var reader = new FileReader();
-        reader.onload = function(e) {
-            ankiBinaryToTable(e.target.result);
-        };
+        reader.onload = function(e) { ankiBinaryToTable(e.target.result); };
         /* // If the callback doesn't need the File object, just use the above.
         reader.onload = (function(theFile) {
             return function(e) {
@@ -118,21 +112,95 @@ $(document).ready(function() {
     };
 
     $("#ankiFile").change(eventHandleToTable);
+
+    ankiURLToTable('/n.apkg');
 });
 
 function core5000Modify(deckNotes, deckFields) {
-    // Very, very sorry for using eval(), but clang-format as of LLVM 3.4.2
-    // doesn't handle Javascript regexps, though it's fixed in Subversion as
-    // of May 2014:
-    // http://lists.cs.uiuc.edu/pipermail/cfe-commits/Week-of-Mon-20140512/105327.html
-    eval('var kanaRegexp = /\\[([あ-んア-ン]+)\\]/g;');
+    var kanaRegexp = XRegExp('\\[([\\p{Hiragana}\\p{Katakana}]+)\\]', 'g');
 
     var abbreviations =
-        "adn.,adv.,aux.,conj.,cp.,i-adj.,interj.,n.,na-adj.,num.,p.,p. "
-        "case,p. conj.,p. disc.,pron.,v.".split(',');
-    var kanaKanji = XRegExp('([\\p{Han}\\p{Katakana}\\p{Hiragana}]+) ');
+        "adn.,adv.,aux.,conj.,cp.,i-adj.,interj.,n.,na-adj.,num.,p.,p. \
+case,p. conj.,p. disc.,pron.,v.".split(',');
+    var abbreviationsOr = abbreviations.join("|").replace(/\./g, '\\.');
 
-    // COMBINE THESE TWO!
+    var kanaKanjiWordRegexp = '([^a-z]+)';
+    var romajiRegexp = '([a-z\\s,\\-()’]+)';
+    var partOfSpeechRegexp = '((?: |,|' + abbreviationsOr + ')+)';
+
+    var bar = function(s) {
+        var arr = s.split(XRegExp('(' + abbreviationsOr + ')'));
+
+        var isAbbreviation = arr.map(
+            function(x) { return abbreviations.indexOf(x) >= 0 ? 1 : 0; });
+        var isWhitePunctuation =
+            arr.map(function(x) { return x.match(/^[\s,]*$/) ? 1 : 0; });
+        var isAbbrOrWhitePunct = isAbbreviation.map(
+            function(x, i) { return x + isWhitePunctuation[i]; });
+
+        var tmp = removeJunk(isAbbrOrWhitePunct, arr);
+        arr = tmp.other_array;
+        isAbbrOrWhitePunct = tmp.this_array;
+
+        var pos = [];
+        var translation = [];
+        arr.map(function(x, i) {
+            if (isAbbrOrWhitePunct[i]) {
+                pos.push(x);
+                translation.push(arr[i + 1]);
+            }
+        });
+
+        var kanaKanjiMatch =
+            s.match(XRegExp(kanaKanjiWordRegexp + ' ' + romajiRegexp + ' ' +
+                            partOfSpeechRegexp));
+        return {
+            pos : pos,
+            translation : translation,
+            word : kanaKanjiMatch[1],
+            romaji : kanaKanjiMatch[2]
+        };
+    };
+
+    function removeJunk(this_array, other_array) {
+        var i = 1;
+        while (i < this_array.length) {
+            if (this_array[i] == this_array[i - 1] && this_array[i] > 0) {
+                this_array.splice(i - 1, 2, 1);
+                other_array.splice(i - 1, 2,
+                                   other_array[i - 1] + other_array[i]);
+            } else {
+                i++;
+            }
+        }
+        return {other_array : other_array, this_array : this_array};
+    }
+
+    function decodeHtml(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+
+    var wordColumnReplace = function(s) {
+        if (s.search("&") >= 0) {
+            s = decodeHtml(s);
+        }
+        var arr = s.match(
+            XRegExp('([^a-z]+)([^\\p{Han}\\p{Katakana}\\p{Hiragana}]+)', 'g'));
+
+        return arr.map(function(s) {
+            var decomp = bar(s);
+            var posTrans = decomp.pos.map(function(pos, i) {
+                return '<span class="part-of-speech">' + pos +
+                       '</span> <span class="target-words-meaning">' +
+                       decomp.translation[i] + '</span>';
+            }).join(" ");
+            return '<span class="target-words">' + decomp.word +
+                   '</span> <span class="target-words-romaji">' +
+                   decomp.romaji + " " + posTrans + '</span>'
+        }).join("");
+    };
 
     deckNotes.map(function(note, loc, arr) {
         // Replace [kana] with spans
@@ -140,6 +208,9 @@ function core5000Modify(deckNotes, deckFields) {
                                             function(match, kana, offset, str) {
             return '<span class="reading kana">' + kana + '</span>';
         });
+
+        // Break up Word column into its four separate components
+        note.Word = wordColumnReplace(note.Word);
 
         return note;
     });
